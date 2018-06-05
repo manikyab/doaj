213085e41d1acd5638423365a25c3ca679422790
manikyab <manikyab@users.noreply.github.com>
2018-06-05 16:48:59 +0530

    Update csv_entry.py

--- csv_entry.py
+++ csv_entry.py
@@ -1,11 +1,9 @@
-#python 3
-
 import MySQLdb as sql
 import csv 
 import urllib.request
 
 
-conn=sql.connect('localhost','python','python','journal')
+conn=sql.connect('localhost','root','','journal')
 cur=conn.cursor()
 
 #urllib.request.urlretrieve ("https://doaj.org/csv", "data.csv") #saving doaj_db file in data.csv
@@ -17,6 +15,11 @@ reader = csv.DictReader(csvfile) #iterating line by line from the data.csv
 flag=0
 
 for row in reader:
+	for i in z:
+		if "'" in row[i]:
+			temp=row[i]
+			tem=temp.index("'")
+			row[i]=temp[:tem]+"'"+temp[tem:]
     
     #title=row[z[0]]
     #eissn=row[z[1]]
@@ -35,10 +38,11 @@ for row in reader:
     date=row[z[12]]
     query = "insert into doaj values('"+row[z[0]]+"','"+row[z[1]]+"','"+row[z[2]]+"','"+row[z[3]]+"','"+row[z[4]]+"','"+row[z[5]]+"','"+row[z[6]]+"','"+row[z[7]]+"','"+row[z[8]]+"','"+row[z[9]]+"','"+row[z[10]]+"','"+row[z[11]]+"','"+date[0:10]+"','"+row[z[13]]+"')"
     query=query.encode("utf8")
-    #print (query)
+    print (query)
     cur.execute(query) 
     conn.commit()
-    #flag=flag+1
+    flag=flag+1
+    print (flag)
     #if flag==15:
      #   break
 csvfile.close()
