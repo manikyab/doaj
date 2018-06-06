070b10894092b5e37fe5f641f033f66a6c37f696
manikyab <manikyab@users.noreply.github.com>
2018-06-06 11:36:17 +0530

    Update csv_entry2.py

--- csv_entry2.py
+++ csv_entry2.py
@@ -22,7 +22,7 @@ conn=sql.connect('localhost','root','','journal')
 cur=conn.cursor()
 
 #urllib.request.urlretrieve ("https://doaj.org/csv", "data.csv") #saving doaj_db file in data.csv
-z=['Journal title','Journal EISSN (online version)','Journal URL','Alternative title','Journal ISSN (print version)','Publisher','Society or institution','Platform, host or aggregator','Country of publisher','Full text formats','Keywords','Full text language','Added on Date','Subjects'] #attributes 
+z=['Journal title','Journal EISSN (online version)','Journal URL','Alternative title','Journal ISSN (print version)','Publisher','Society or institution','Platform, host or aggregator','Country of publisher','Full text formats','Keywords','Full text language','First calendar year journal provided online Open Access content','Added on Date','Subjects'] #attributes 
 
 csvfile = open('data.csv',encoding="utf8") #open the file with binary encoding
 	
@@ -44,10 +44,11 @@ for row in reader:
     #formats=row[z[9]]
     #keyword=row[z[10]]
     #language=row[z[11]]
-    #date=row[z[12]]
-    #sunject=row[z[13]]
-    date=row[z[12]]
-    query = "insert into doaj values('"+row[z[0]]+"','"+row[z[1]]+"','"+row[z[2]]+"','"+row[z[3]]+"','"+row[z[4]]+"','"+row[z[5]]+"','"+row[z[6]]+"','"+row[z[7]]+"','"+row[z[8]]+"','"+row[z[9]]+"','"+row[z[10]]+"','"+row[z[11]]+"','"+date[0:10]+"','"+row[z[13]]+"')"
+    #Date_Open=row[z[12]]
+    #date=row[z[13]]
+    #sunject=row[z[14]]
+    date=row[z[13]]
+    query = "insert into doaj values('"+row[z[0]]+"','"+row[z[1]]+"','"+row[z[2]]+"','"+row[z[3]]+"','"+row[z[4]]+"','"+row[z[5]]+"','"+row[z[6]]+"','"+row[z[7]]+"','"+row[z[8]]+"','"+row[z[9]]+"','"+row[z[10]]+"','"+row[z[11]]+"','"+row[z[12]]+"','"+date[0:10]+"','"+row[z[14]]+"')"
     query=query.encode("utf8")
     print (query)
     cur.execute(query) 
