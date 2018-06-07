a83f3e35be20f8f911a4ecc4e7753050503aff35
ravi goyal <39941458+Goyalravi97@users.noreply.github.com>
2018-06-07 15:15:54 +0530

    Update jlist_entry.py

--- jlist_entry.py
+++ jlist_entry.py
@@ -1,27 +1,27 @@
 import MySQLdb as sql
 import csv
 
-conn=sql.connect('localhost','root','','journal')
+conn=sql.connect('localhost','python','python','journal')
 cur=conn.cursor()
 
 csvfile1 = open('jlist.csv',encoding="utf8")
 
 reader1 = csv.DictReader(csvfile1)
-x=['Journal title','eISSN','Journal URL','pISSN','Publisher']
+x=['Journal title','eISSN','pISSN','Publisher','Latest issue','Journal URL']
 for row1 in reader1:
-    query1=("select * from doaj where EISSN=row1[x[1]] OR ISSN=row1[x[3]]")
+    query1="select * from doaj where EISSN='"+row1[x[1]]+"' OR ISSN='"+row1[x[3]]+"'"
     cur.execute(query1)
     check=cur.rowcount
+    print(row1[x[2]])
 
     if check==0:
 
-        query = ("insert into doaj""('Title','EISSN','URL','ISSN','Publisher','Add_Date')" "values('"+row1[x[0]]+"','"+row1[x[1]]+"','"+row1[x[2]]+"','"+row1[x[3]]+"','"+row1[x[4]]+"','"+row1[x[5]]+"')")
+        query = "insert into doaj('Title','EISSN','URL','ISSN','Publisher','Add_Date') values('"+row1[x[0]]+"','"+row1[x[1]]+"','"+row1[x[5]]+"','"+row1[x[2]]+"','"+row1[x[3]]+"','"+row1[x[4]]+"')"
         query=query.encode("utf8")
-        print(query)
+        print (query)
         cur.execute(query) 
         conn.commit()
 
 csvfile1.close()
 cur.close()
 conn.close()
-
