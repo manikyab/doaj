18d84ccc92a8f926a9749e9462cc9ecb81321c8e
devanshnema14 <39941395+devanshnema14@users.noreply.github.com>
2018-06-07 10:56:07 +0530

    Update csv_entry2.py

--- csv_entry2.py
+++ csv_entry2.py
@@ -21,13 +21,13 @@ def quotes(row):
 conn=sql.connect('localhost','root','','journal')
 cur=conn.cursor()
 
-#urllib.request.urlretrieve ("https://doaj.org/csv", "data.csv") #saving doaj_db file in data.csv
+urllib.request.urlretrieve ("https://doaj.org/csv", "data.csv") #saving doaj_db file in data.csv
 z=['Journal title','Journal EISSN (online version)','Journal URL','Alternative title','Journal ISSN (print version)','Publisher','Society or institution','Platform, host or aggregator','Country of publisher','Full text formats','Keywords','Full text language','First calendar year journal provided online Open Access content','Added on Date','Subjects'] #attributes 
 
 csvfile = open('data.csv',encoding="utf8") #open the file with binary encoding
 	
 reader = csv.DictReader(csvfile) #iterating line by line from the data.csv
-flag=0
+
 
 for row in reader:
     
@@ -50,13 +50,11 @@ for row in reader:
     date=row[z[13]]
     query = "insert into doaj values('"+row[z[0]]+"','"+row[z[1]]+"','"+row[z[2]]+"','"+row[z[3]]+"','"+row[z[4]]+"','"+row[z[5]]+"','"+row[z[6]]+"','"+row[z[7]]+"','"+row[z[8]]+"','"+row[z[9]]+"','"+row[z[10]]+"','"+row[z[11]]+"','"+row[z[12]]+"','"+date[0:10]+"','"+row[z[14]]+"')"
     query=query.encode("utf8")
-    print (query)
+    
     cur.execute(query) 
     conn.commit()
-    flag=flag+1
-    print (flag)
-    if flag==15:
-        break
+   
+    
 csvfile.close()
 cur.close()
 conn.close()
