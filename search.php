14e2ff586457b4cd04f7da9c7c5937d7f72f9f10
davidjindal <34886483+davidjindal@users.noreply.github.com>
2018-06-07 14:21:37 +0530

    Update search.php

--- search.php
+++ search.php
@@ -18,8 +18,11 @@ else
 		$user="python";
 		$pass="python";
 		$db="journal";
-		mysql_connect($host, $user, $pass);
-		mysql_select_db($db);
+		$conn=mysqli_connect($host, $user, $pass, $db);
+		if (!$conn)
+		{
+    		die("Connection failed: " . mysqli_connect_error());
+		}
 		$search_explode=explode(" ", $input);
 		$x=0;
 		foreach ($search_explode as $search_each) 
@@ -36,8 +39,8 @@ else
 			}
 		}	
 		$construct="SELECT * FROM doaj WHERE $construct";
-		$res=mysql_query($construct);
-		$num=mysql_num_rows($res);
+		$res=mysqli_query($conn, $construct);
+		$num=mysqli_num_rows($res);
 		if($num==0)
 		{
 			echo "There are no matching result for <b> $input </b>.<br><br> 1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' <br> 2. Try different words with similar meaning <br> 3. Please check your spelling";
@@ -45,7 +48,7 @@ else
 		else
 		{
 			echo "$num results found";
-			while($rows=mysql_fetch_assoc($res))
+			while($rows=mysqli_fetch_assoc($res))
 			{
 				$title=$row ['Title'];
 				$url=$row ['URL'];
@@ -60,4 +63,5 @@ else
 		}
 	}
 }
+mysqli_close($conn);
 ?>
