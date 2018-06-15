208359cd7dc4d9a1061a53e45e23abef7cb157d3
ravi goyal <39941458+Goyalravi97@users.noreply.github.com>
2018-06-15 12:41:00 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,7 +1,12 @@
 <?php
+include("headfoot.html")
+?>		
+<?php
  header('Content-type: text/html; charset=utf-8');
 $button=$_POST['submit'];
 $input=$_POST['in'];
+
+
 if(!$button)
 {
 	echo "you didn't submit";
@@ -40,15 +45,18 @@ else
 			}
 		}	*/
 		$construct= "SELECT * FROM article WHERE (title LIKE '%$input%' OR author LIKE '%$input%')";
+		$construct1= "SELECT * FROM doaj WHERE (Title LIKE '%$input%' OR ISSN LIKE '%$input%' OR EISSN LIKE '%$input%' OR Keywords LIKE '%$input%' OR Subjects LIKE '%$input%' OR Publisher LIKE '%$input%')";
+
 		$res=mysqli_query($conn, $construct);
+		$res1=mysqli_query($conn, $construct1);
 		$num=mysqli_num_rows($res);
 		if($num==0)
 		{
-			echo "There are no matching result for <b> $input </b>.<br><br> 1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' <br> 2. Try different words with similar meaning <br> 3. Please check your spelling";
+			echo "No Article Availabe For $input <br>";
 		}
 		else
 		{
-			echo "$num results found <br><br><br>";
+			echo "$num results found for articles <br><br>";
 			while($rows=mysqli_fetch_assoc($res))
 			{
 				$title=$rows ['title'];
@@ -61,7 +69,36 @@ else
 				$iss=$rows ['issue'];
 				$pdf=$rows ['pdf_link'];
 				$lim=substr($sum, 0, 250);
-				echo "<a href='$link'> <b> $title </b> </a> <br> <a href='$link'> $link </a> <br><a href='$jlink'><b>Journal:- $jtitle </b></a> <br>Author:- $author &nbsp&nbsp&nbsp Volume:- $vol &nbsp&nbsp&nbsp Issue:- $iss<br>Summary:- $lim....<br><a href='$pdf'>Click here</a>for PDF <br><br>";
+				if($pdf=="")
+				{
+					echo "<a href='$link'> <b> $title </b> </a>  <br><a href='$jlink'><b>Journal:- $jtitle </b></a> <br>Author:- $author &nbsp&nbsp&nbsp Volume:- $vol &nbsp&nbsp&nbsp Issue:- $iss<br>Summary:- $lim....<br> PDF link not available <br><br><br>";
+				}
+				else{
+					echo "<a href='$link'> <b> $title </b> </a>  <br><a href='$jlink'><b>Journal:- $jtitle </b></a> <br>Author:- $author &nbsp&nbsp&nbsp Volume:- $vol &nbsp&nbsp&nbsp Issue:- $iss<br>Summary:- $lim....<br><a href='$pdf'>Click here</a>for PDF <br><br><br>";
+				}
+			}
+				
+		}
+		$num1=mysqli_num_rows($res1);
+		if($num1==0)
+		{
+			echo "No Journals Availabe For $input";
+		}
+		else
+		{
+			echo "$num1 results found journals <br><br>";
+			while($rows1=mysqli_fetch_assoc($res1))
+			{
+				$title=$rows1 ['Title'];
+				$url=$rows1 ['URL'];
+				$issn=$rows1 ['ISSN'];
+				$eissn=$rows1 ['EISSN'];
+				$date=$rows1 ['Add_Date'];
+				$sub=$rows1 ['Subjects'];
+				$pub=$rows1 ['Publisher'];
+				$key=$rows1 ['Keywords'];
+				echo "<a href='$url'> <b> $title </b> </a> <br> <a href='$url'> $url </a> <br>keywords:- $key <br>ISSN:- $issn <br>EISSN:- $eissn <br>Subject:- $sub <br>Publisher:- $pub<br><br>";
+				#printf("%s %s", $rows["Title"],$rows["URL"]);
 			}
 		}
 	}
