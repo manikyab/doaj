b030184664f7e79d15129464af6ec6b538ab0080
ravi goyal <39941458+Goyalravi97@users.noreply.github.com>
2018-06-15 15:26:09 +0530

    Add files via upload

---
+++ display1.php
@@ -0,0 +1,81 @@
+
+<?php
+include("headfoot.html")
+?>	
+<?php
+ header('Content-type: text/html; charset=utf-8');
+$button=$_POST['submit'];
+$input=$_POST['in'];
+if(!$button)
+{
+	echo "you didn't submit";
+}
+else
+{
+	if(strlen($input)<=1)
+	{
+		echo "Search term is too short";
+	}
+	else
+	{
+		echo "you searched for <b> $input </b> <hr size='1'><br>";
+		$host="192.168.1.48";
+		$user="python";
+		$pass="python";
+		$db="journal";
+		$conn=mysqli_connect($host, $user, $pass, $db);
+		if (!$conn)
+		{
+    		die("Connection failed: " . mysqli_connect_error());
+		}
+		/*$search_explode=explode(" ", $input);
+		$x=0;
+		foreach ($search_explode as $search_each) 
+		{
+			$x++;
+			$construct="";
+			if($x==1)
+			{
+				$construct .="(Title LIKE '%$search_each%')";
+			}
+			else
+			{
+				$construct .="(OR ISSN LIKE '%$search_each%' OR EISSN LIKE '%$search_each%' OR Keywords LIKE '%$search_each%' OR Subjects LIKE '%$search_each%' OR Publisher LIKE '%$search_each%')";
+			}
+		}	*/
+		$construct= "SELECT * FROM article WHERE (title LIKE '%$input%' OR author LIKE '%$input%')";
+		$res=mysqli_query($conn, $construct);
+		$num=mysqli_num_rows($res);
+		if($num==0)
+		{
+			echo "There are no matching result for <b> $input </b>.<br><br> 1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' <br> 2. Try different words with similar meaning <br> 3. Please check your spelling";
+		}
+		else
+		{
+			echo "$num results found <br><br><br>";
+			while($rows=mysqli_fetch_assoc($res))
+			{
+				$title=$rows ['title'];
+				$link=$rows ['link'];
+				$jlink=$rows ['j_link'];
+				$jtitle=$rows ['j_title'];
+				$author=$rows ['author'];
+				$sum=$rows ['summary'];
+				$vol=$rows ['volume'];
+				$iss=$rows ['issue'];
+				$pdf=$rows ['pdf_link'];
+				$lim=substr($sum, 0, 250);
+				if($pdf=="")
+				{
+					echo "<a href='$link'> <b> $title </b> </a>  <br><a href='$jlink'><b>Journal:- $jtitle </b></a> <br>Author:- $author &nbsp&nbsp&nbsp Volume:- $vol &nbsp&nbsp&nbsp Issue:- $iss<br>Summary:- $lim....<br> PDF link not available <br><br><br>";
+				}
+				else{
+					echo "<a href='$link'> <b> $title </b> </a>  <br><a href='$jlink'><b>Journal:- $jtitle </b></a> <br>Author:- $author &nbsp&nbsp&nbsp Volume:- $vol &nbsp&nbsp&nbsp Issue:- $iss<br>Summary:- $lim....<br><a href='$pdf'>Click here</a>for PDF <br><br><br>";
+				}
+				
+			}
+		}
+	}
+}
+#mysqli_close($conn);
+?>
